c3cb428867d306cbbc84e3ac93c272f3e891d116
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-05 09:58:00 +0400

    added shadcn UI components

---
+++ components/ui/Heading.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface HeadingProps {
+  title: string;
+  description: string;
+}
+const { title, description } = defineProps<HeadingProps>();
+</script>
+
+<template>
+  <div>
+    <h2 class="text-3xl font-bold tracking-tight">{{ title }}</h2>
+    <p class="text-sm text-muted-foreground">
+      {{ description }}
+    </p>
+  </div>
+</template>
---
+++ components/ui/Modal.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+const emit = defineEmits(["close"]);
+
+interface ModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+}
+
+const { title, description, isOpen } = defineProps<ModalProps>();
+
+const onChange = (open: boolean) => {
+  if (!open) {
+    emit("close");
+  }
+};
+</script>
+
+<template>
+  <Dialog :open="isOpen" @update:open="onChange(false)">
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>{{ title }}</DialogTitle>
+        <DialogDescription> {{ description }} </DialogDescription>
+      </DialogHeader>
+      <div><slot /></div>
+    </DialogContent>
+  </Dialog>
+</template>
