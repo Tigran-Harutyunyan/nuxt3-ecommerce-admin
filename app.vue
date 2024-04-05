36f23e609b43159121d5661ea32503d2106ef25f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-05 12:03:39 +0400

    app.vue

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Toaster } from "@/components/ui/sonner";
+import ModalsContainer from "@/components/modals/ModalsContainer.vue";
+import { ClerkLoaded, ClerkLoading } from "vue-clerk";
+</script>
+<template>
+  <NuxtLoadingIndicator color="#4ade80" :height="6" />
+
+  <ClerkLoaded>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+    <Toaster />
+    <ModalsContainer />
+  </ClerkLoaded>
+</template>
