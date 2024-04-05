b5d8bc8d8fece4e3ae0641ad7bf6430ad9e427ff
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-05 12:02:56 +0400

    created NavBar component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import { UserButton } from "vue-clerk";
+
+const { data: stores, pending } = await useLazyAsyncData("stores", () =>
+  $fetch("/api/stores")
+);
 </script>
 
 <template>
   <div class="border-b">
     <div class="flex h-16 items-center px-4">
-      <!-- <StoreSwitcher items="{stores}" /> -->
+      <StoreSwitcher :items="stores" v-if="stores && Array.isArray(stores)" />
       <MainNav className="mx-6" />
       <div class="ml-auto flex items-center space-x-4">
-        <!-- <ThemeToggle /> -->
+        <ThemeToggle />
         <UserButton afterSignOutUrl="/" />
       </div>
     </div>
