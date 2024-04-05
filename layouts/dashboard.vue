916ef9debbae3f614d30ea2107c5e9f88237c4fc
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-05 12:03:12 +0400

    refactored dashboard layout

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
 import { useClerkProvide } from "vue-clerk";
-import { useModals } from "@/store/modals";
 
 const { derivedState } = useClerkProvide();
 
 const route = useRoute();
 
-const { onOpen } = useModals();
-
 if (!derivedState.value.userId) {
   navigateTo("/sign-in");
 }
 
-const { data: store, pending } = await useLazyAsyncData("store", () =>
+const { data: store } = await useLazyAsyncData("store", () =>
   $fetch(`/api/store/${route.params.storeId}`)
 );
 
