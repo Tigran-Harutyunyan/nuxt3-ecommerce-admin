c4f3db579f81982be9005398809cbc4cc0ddb3eb
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-05 09:54:15 +0400

    created index layout

---
+++ layouts/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { data: store } = await useLazyAsyncData("store", () =>
+  $fetch("/api/store")
+);
+debugger;
+if (store) {
+  navigateTo(`/${store.id}`);
+}
+</script>
+<template>
+  <slot />
+</template>
