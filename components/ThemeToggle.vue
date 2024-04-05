be0b69f5b5f73da1850ef3950b905b8b4c2f7be3
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-05 12:02:44 +0400

    created ThemeToggle component to control dark mode

---
+++ components/ThemeToggle.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoonIcon, SunIcon } from "lucide-vue-next";
+import { useColorMode } from "@vueuse/core";
+
+const mode = useColorMode({
+  emitAuto: true,
+  modes: {
+    dark: "dark",
+    light: "",
+  },
+});
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline" size="icon">
+        <SunIcon
+          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <MoonIcon
+          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
+        <span class="sr-only">Toggle theme</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="mode = 'light'"> Light </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'dark'"> Dark </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
