84a66fbbab73f9f8355f4e035568f6a145c6bc76
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-06 10:06:51 +0400

    created ApiList component

---
+++ components/ui/ApiList.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import ApiAlert from "@/components/ui/ApiAlert.vue";
+
+interface ApiListProps {
+  entityName: string;
+  entityIdName: string;
+}
+const route = useRoute();
+const props = defineProps<ApiListProps>();
+const { entityName, entityIdName } = toRefs(props);
+
+const baseUrl = `${location.origin}/api/store/${route.params.storeId}`;
+</script>
+
+<template>
+  <ApiAlert
+    title="GET"
+    variant="public"
+    :description="`${baseUrl}/${entityName}`"
+  />
+  <ApiAlert
+    title="GET"
+    variant="public"
+    :description="`${baseUrl}/${entityIdName}`"
+  />
+  <ApiAlert
+    title="POST"
+    variant="admin"
+    :description="`${baseUrl}/${entityName}`"
+  />
+  <ApiAlert
+    title="PATCH"
+    variant="admin"
+    :description="`${baseUrl}/${entityIdName}`"
+  />
+  <ApiAlert
+    title="DELETE"
+    variant="admin"
+    :description="`${baseUrl}/${entityIdName}`"
+  />
+</template>
