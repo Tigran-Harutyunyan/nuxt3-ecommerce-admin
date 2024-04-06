678bc716c070becda54a3b0d4bdc25ca569f6dc4
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-06 10:13:37 +0400

    created endpoint for getting billboards

---
+++ server/api/store/[storeId]/billboards/index.get.ts
@@ -0,0 +1,36 @@
+import { clerkClient } from 'h3-clerk'
+import prismadb from '@/lib/prismadb';
+
+export default defineEventHandler(async (event) => {
+    const { auth } =  event.context;
+    const params = await event.context.params
+
+    if (!auth.userId) {
+        setResponseStatus(event, 403)
+        return ''
+    }
+
+    if (!params.storeId) {
+        return  createError({
+            status: 400,
+            statusMessage: 'Store ID is required'
+        });
+    } 
+
+    try {
+        const billboards = await prismadb.billboard.findMany({
+            where: {
+              storeId: params.storeId
+            },
+            orderBy: {
+              createdAt: 'desc'
+            }
+          });
+          return billboards;
+
+    } catch (error) {
+        return {
+            error: error?.message
+        }
+    }
+});
