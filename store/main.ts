e9423e8583e05f12158b0b282706e62245c1e208
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-06 19:29:10 +0400

    refactored store

--- store/main.ts
+++ store/main.ts
@@ -2,13 +2,20 @@ import { defineStore } from "pinia";
 
 export const useMain = defineStore("main", () => {
   const entityUpdateTrigger = ref(0);
+  const storeUpdateTrigger = ref(0);
 
-  const setEntityTrigger = () => {
+  const updateEntityTrigger = () => {
     entityUpdateTrigger.value++;
+  }
+
+  const updateStoreTrigger = () => {
+    storeUpdateTrigger.value++;
   }
 
   return {
     entityUpdateTrigger,
-    setEntityTrigger
+    storeUpdateTrigger,
+    updateEntityTrigger,
+    updateStoreTrigger
   };
 });
