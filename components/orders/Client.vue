6ebfdc57e56ced3e70634e13ae6596384fad53ae
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-08 09:29:00 +0400

    created Client component for orders

---
+++ components/orders/Client.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { Separator } from "@/components/ui/separator";
+import DataTable from "@/components/ui/DataTable.vue";
+import Heading from "@/components/ui/Heading.vue";
+import type { OrderColumn } from "@/types";
+import { type ColumnDef } from "@tanstack/vue-table";
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+const { data } = defineProps<OrderClientProps>();
+const query = useRoute();
+
+const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "products",
+    header: "Products",
+  },
+  {
+    accessorKey: "phone",
+    header: "Phone",
+  },
+  {
+    accessorKey: "address",
+    header: "Address",
+  },
+  {
+    accessorKey: "totalPrice",
+    header: "Total price",
+  },
+  {
+    accessorKey: "isPaid",
+    header: "Paid",
+  },
+];
+</script>
+
+<template>
+  <Heading
+    :title="`Orders (${data?.length})`"
+    description="Manage orders for your store"
+  />
+
+  <Separator />
+  <DataTable searchKey="products" :columns="columns" :data="data" />
+  <Separator />
+</template>
